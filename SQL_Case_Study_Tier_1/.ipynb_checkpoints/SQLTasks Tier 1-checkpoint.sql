4b93e8f8f188a6e46c913caa23a6f4bebe40e1ba
gawantang <gawantang@hotmail.com>
2025-06-26 15:55:04 -0400

    updated country club assignment files

--- SQL_Case_Study_Tier_1/.ipynb_checkpoints/SQLTasks Tier 1-checkpoint.sql
+++ SQL_Case_Study_Tier_1/.ipynb_checkpoints/SQLTasks Tier 1-checkpoint.sql
@@ -88,12 +88,115 @@ WHERE joindate = (
     );
 # can't just do WHERE joindate = MAX(joindate) because aggregate functions like MAX() can't be used directly in WHERE clauses. The subquery lets you first calculate the maximum date, then use that value for comparison.
 
+
+ ************** Run following questions through python  **************
+
 /* Q7: Produce a list of all members who have used a tennis court.
 Include in your output the name of the court, and the name of the member
 formatted as a single column. Ensure no duplicate data, and order by
 the member name. */
 
-RE-START HERE
+draft code:
+
+Attempt 1:
+    # the clause - SELECT CONCAT(firstname,' ', surname) AS full_name
+    # works in MySQL but not in SQLite
+    # My permission level doesn't seem to allow nested queries such as the one below and returns a 403 error
+    SELECT DISTINCT firstname || ' ' || surname AS unique_full_name
+    FROM Members 
+    WHERE memid IN 
+    	(SELECT memid
+         FROM Bookings
+         WHERE facid IN
+    		(SELECT facid 
+    		FROM Facilities
+    		WHERE name LIKE 'Tennis%'))
+    ORDER BY surname, firstname;
+
+# Attempt 2:
+    # try splitting into two queries
+    SELECT facid FROM Facilities WHERE name LIKE 'Tennis%';
+    
+    SELECT DISTINCT firstname || ' ' || surname AS unique_full_name
+    FROM Members 
+    WHERE memid IN 
+        (SELECT memid 
+         FROM Bookings 
+         WHERE facid IN (0, 1))
+    ORDER BY unique_full_name;
+
+# Attempt 3:
+    # even a single subquery doesn't work, try using JOIN
+    SELECT DISTINCT firstname || ' ' || surname AS unique_full_name, f.name
+    FROM Members m
+    JOIN Bookings b ON m.memid = b.memid
+    JOIN Facilities f ON b.facid = f.facid
+         WHERE f.name LIKE 'Tennis%'
+    ORDER BY unique_full_name;
+
+# Attempt 4
+    # I could not combine the first name and surname. Using CONCAT triggered a 403 access error.
+    # and using the || symbols returned a zero value, as if the || symbols were interpretted incorrectly.
+    
+    SELECT DISTINCT firstname, surname, f.name
+    FROM Members m
+    JOIN Bookings b ON m.memid = b.memid
+    JOIN Facilities f ON b.facid = f.facid
+    WHERE f.name LIKE 'Tennis%'
+    ORDER BY surname, firstname;
+
+    # note that DISTINCT works on an entire row of data, removing any duplicates of firstname and surname
+    # no need to add parentheses around those field names
+
+From David: *** doesn't work, throws 403 error
+                SELECT f.name AS facility_name, 
+                CONCAT(m.firstname, ' ', m.surname) AS member_name, cost.total_cost
+                FROM Bookings b
+                JOIN Facilities f ON b.facid = f.facid
+                JOIN Members m ON b.memid = m.memid
+                JOIN 
+                        (SELECT 
+                            bookid, 
+                                (CASE 
+                                    WHEN b.memid = 0 THEN f.guestcost * b.slots  
+                                    ELSE f.membercost * b.slots  
+                                END) AS total_cost
+                            FROM Bookings b
+                        JOIN Facilities f ON b.facid = f.facid
+                        ) cost ON b.bookid = cost.bookids
+                WHERE b.starttime LIKE '2012-09-14%'  
+                AND cost.total_cost > 30  -- Filtering bookings that exceed $30
+                ORDER BY cost.total_cost DESC;
+
+
+From the TA: **** works except for the name combination part
+
+                SELECT
+                    firstname || ' ' || surname AS member,
+                    name AS facility,
+                    cost
+                FROM (
+                    SELECT
+                        firstname,
+                        surname,
+                        name,
+                        CASE
+                            WHEN firstname = 'GUEST' THEN guestcost * slots
+                            ELSE membercost * slots
+                        END AS cost,
+                        starttime
+                    FROM Members
+                    INNER JOIN Bookings ON Members.memid = Bookings.memid
+                    INNER JOIN Facilities ON Bookings.facid = Facilities.facid
+                ) AS inner_table
+                WHERE starttime >= '2012-09-14'
+                  AND starttime < '2012-09-15'
+                  AND cost > 30
+                ORDER BY Member
+
+# testing double pipe syntax
+SELECT DISTINCT firstname || ' ' || surname AS unique_full_name
+    FROM Members m
 
 /* Q8: Produce a list of bookings on the day of 2012-09-14 which
 will cost the member (or guest) more than $30. Remember that guests have
@@ -102,9 +205,61 @@ the guest user's ID is always 0. Include in your output the name of the
 facility, the name of the member formatted as a single column, and the cost.
 Order by descending cost, and do not use any subqueries. */
 
+final:
+    SELECT name, firstname, surname,
+        CASE
+            WHEN firstname = 'GUEST' THEN (guestcost * slots)
+            ELSE (membercost * slots)
+        END AS cost
+    FROM Bookings AS b
+    LEFT JOIN Facilities AS f ON f.facid = b.facid
+    LEFT JOIN Members as m USING (memid) 
+    WHERE b.starttime BETWEEN '2012-09-14 00:00:00' AND '2012-09-14 23:59:59'
+    HAVING cost > 30;
+
+
+
 
 /* Q9: This time, produce the same result as in Q8, but using a subquery. */
 
+Attempt 1: this resulted in an error code 403
+    SELECT name, firstname, surname,
+        CASE
+            WHEN firstname = 'GUEST' THEN (guestcost * slots)
+            ELSE (membercost * slots)
+        END AS cost
+    FROM Bookings
+    WHERE starttime BETWEEN '2012-09-14 00:00:00' AND '2012-09-14 23:59:59' 
+        AND facid IN
+            (SELECT name, membercost, guestcost, facid
+            FROM Facilities
+            WHERE facid IN
+                (SELECT memid, starttime, slots
+                FROM Bookings))
+    HAVING cost > 30;
+
+Attempt 2: still running into error code 403
+    # the following query will run fine if the WHERE clause is removed
+    # the WHERE clause works fine if it's not combined with the entire query
+    # possibly a permissions issue that prevents subqueries in combination with WHERE and HAVING?
+
+    SELECT 
+        (SELECT name 
+        FROM Facilities 
+        WHERE facid = b.facid) AS name,
+        (SELECT firstname 
+        FROM Members 
+        WHERE memid = b.memid) AS firstname,
+        (SELECT surname 
+        FROM Members 
+        WHERE memid = b.memid) AS surname,
+        AND 
+        (CASE
+            WHEN (SELECT firstname FROM Members WHERE memid = b.memid) = 'GUEST' 
+            THEN (SELECT guestcost FROM Facilities WHERE facid = b.facid) * b.slots
+            ELSE (SELECT membercost FROM Facilities WHERE facid = b.facid) * b.slots
+        END AS cost)>30
+    FROM Bookings b
 
 /* PART 2: SQLite
 /* We now want you to jump over to a local instance of the database on your machine. 
